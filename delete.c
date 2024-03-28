427af27b5285bd468aacd6d5cca1d12a8f443b93
El Junior <junioreyetemou@gmail.com>
2024-03-28 14:46:19 +1000

    mise a jour des fichiers

--- delete.c
+++ delete.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "delete_name.h"
+
 void delete_name(FILE* fichier, char* name)
 {
 
--- delete.h
+++ delete.h
@@ -1,2 +1,5 @@
 // prototype de la fonction delete
+#ifndef delete
+#define delete
 void delete_name(FILE* fichier, char* name);
+#endif
